a8a136553b9288ade2202b756f7d8879c47ad3e4
oshanQQ <oshanqq@gmail.com>
2021-07-27 16:21:24 +0900

    📝 Implement reading a line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::io;
+
+fn shelly_loop() {
+    let arguments: String;
+    let status: bool;
+
+    loop {
+        print!("> ");
+
+        // Read line
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line)
+            .expect("Error: Failed to read a line.");
+
+        arguments = split_line();
+        status = execute(arguments);
+        if (!status) {
+            break;
+        }
+    }
+}
+
+// fn read_line() ->String {
+//     let buffer_size: i32 = 1024;
+//     let position: i32 = 0;
+//     let buffer = Box::new(buffer_size);
+//     let mut c: i32 = 0;
+
+//     loop {
+//         // Read a character
+//         c = getchar();
+
+//         // If we hit EOF, replace it with a null character and return.
+//         if (c == )
+//     }
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let input: Vec<String> = env::args().collect();
+    shelly_loop();
 }
