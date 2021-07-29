559fdc760de1c1301fa4f7b6fe3a4bae2aba0011
oshanQQ <oshanqq@gmail.com>
2021-07-29 14:04:09 +0900

    ♻️ Refactor the reading part

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,6 @@ use std::io;
 
 fn main() {
     let mut input_line = String::new();
-    match io::stdin().read_line(&mut input_line) {
-        Ok(n) => {
-            println!("{} bytes read", n);
-            println!("Input: {}", &input_line);
-        }
-        Err(error) => {
-            println!("Error: {}", error);
-        }
-    }
+    io::stdin().read_line(&mut input_line).expect("Shelly: Input error.");
+    println!("Input: {}", input_line);
 }
