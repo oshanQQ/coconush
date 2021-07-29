1bd6770ef90a24658c84708bda617f211f02a225
oshanQQ <oshanqq@gmail.com>
2021-07-29 15:16:14 +0900

    ✨ The input can be parsed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::io;
 
 fn main() {
+    // Read input line
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).expect("Shelly: Input error.");
-    println!("Input: {}", input_line);
+
+    // Parse input line
+    let command: Vec<&str> = input_line.split_whitespace().collect();
+    
+    for argument in command {
+        println!("{:?}", argument);
+    }
+    
 }
