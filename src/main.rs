ed0eb3f1082e57d829b9b0c1462912066a94f384
oshanQQ <oshanqq@gmail.com>
2021-07-29 15:50:52 +0900

    📝 Easy comment

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ fn main() {
     io::stdin().read_line(&mut input_line).expect("Shelly: Input error.");
 
     // Parse input line
+    // "foo bar baz" => ["foo", "bar", baz]
     let command: Vec<&str> = input_line.split_whitespace().collect();
     
+    // Print debug for vector
     for argument in command {
         println!("{:?}", argument);
     }
